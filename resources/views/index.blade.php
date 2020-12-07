a70570ff185361e662f085b3a5eec643e9b04032
enriko <ecenriko@gmail.com>
2020-12-07 01:31:53 -0600

    uts_laravel

---
+++ resources/views/index.blade.php
@@ -0,0 +1,15 @@
+@extends('Layout/main')
+
+@section('title','web riko')
+
+@section('container')
+ <div class="container">
+     <div class="row">
+        <div class="col-10">
+    <h1 class="mt-4">Hello, world!</h1>
+   </div>
+  </div>
+</div>
+@endsection
+
+
